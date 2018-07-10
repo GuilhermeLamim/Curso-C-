501dd8e90390a4e18bfe2e5e5a5e310ad0cbb30f
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-10 20:40:17 -0300

    Voto pra maior que 16

---
+++ Votar maior que 16.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = 16;
+
+            if (idade < 16)
+            {
+                MessageBox.Show("Você não tem idade para votar.");
+            }
+            else
+            {
+                MessageBox.Show("Você já pode votar.");
+                MessageBox.Show("Cumpra seu papel como cidadão seu bosta.");
+            }
+        }
+    }
+}
